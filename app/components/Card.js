fc2b37d353877f77583ed8dcd5d646536c26703e
jenross <ross.m.jennifer@gmail.com>
2020-03-09 08:34:37 -0400

    refactored card and nav with theme context

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ThemeConsumer } from "../contexts/theme";
+import ThemeContext from "../contexts/theme";
 
 export default function Card({
   header,
@@ -10,22 +10,19 @@ export default function Card({
   name,
   children
 }) {
+  const theme = React.useContext(ThemeContext);
   return (
-    <ThemeConsumer>
-      {theme => (
-        <div className={`card bg-${theme}`}>
-          <h4 className="header-lg center-text">{header}</h4>
-          <img className="avatar" src={avatar} alt={`Avatar for ${name}`} />
-          {subheader && <h4 className="center-text">{subheader}</h4>}
-          <h2 className="center-text">
-            <a className="link" href={href}>
-              {name}
-            </a>
-          </h2>
-          {children}
-        </div>
-      )}
-    </ThemeConsumer>
+    <div className={`card bg-${theme}`}>
+      <h4 className="header-lg center-text">{header}</h4>
+      <img className="avatar" src={avatar} alt={`Avatar for ${name}`} />
+      {subheader && <h4 className="center-text">{subheader}</h4>}
+      <h2 className="center-text">
+        <a className="link" href={href}>
+          {name}
+        </a>
+      </h2>
+      {children}
+    </div>
   );
 }
 
