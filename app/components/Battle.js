2b6a8a5dda2ed3e6c11c57674fa9c917d2865cfe
jenross <ross.m.jennifer@gmail.com>
2020-03-09 08:20:55 -0400

    refactored theme consumer on card battle nav

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -13,7 +13,7 @@ import { Link } from "react-router-dom";
 function Instructions() {
   return (
     <ThemeConsumer>
-      {({ theme }) => (
+      {theme => (
         <div className="instructions-container">
           <h1 className="center-text header-lg">Instructions</h1>
           <ol className="container-sm grid center-text battle-instructions">
@@ -65,7 +65,7 @@ class PlayerInput extends React.Component {
   render() {
     return (
       <ThemeConsumer>
-        {({ theme }) => (
+        {theme => (
           <form className="column player" onSubmit={this.handleSubmit}>
             <label htmlFor="username" className="player-label">
               {this.props.label}
@@ -103,7 +103,7 @@ PlayerInput.propTypes = {
 function PlayerPreview({ username, onReset, label }) {
   return (
     <ThemeConsumer>
-      {({ theme }) => (
+      {theme => (
         <div className="column player">
           <h3 className="player-label">{label}</h3>
           <div className={`row bg-${theme}`}>
